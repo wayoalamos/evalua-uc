c7eeddc9f63fb20becf174a32c8b2faf84da5e66
wayoalamos <malamos2@uc.cl>
2020-04-19 20:39:47 -0400

    add profesor SRMS

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -2,10 +2,12 @@ const projectResolvers = require('./project');
 const userResolvers = require('./user');
 const feedbackResolvers = require('./feedback');
 const campusResolvers = require('./campus');
+const profesorResolvers = require('./profesor');
 
 module.exports = [
   userResolvers,
   projectResolvers,
   feedbackResolvers,
   campusResolvers,
+  profesorResolvers,
 ];
